241b5bb251f7c26b592f2646c07e7a87ac42cf87
Benson Ninh <bensonninh@gmail.com>
2017-03-21 21:49:23 -0700

    completes all tests in a different format codefights says new implementation is slower than the one that uses splice old implementation exists at the bottom of the code, commented out

--- codefights/almostIncreasingSequence.js
+++ codefights/almostIncreasingSequence.js
@@ -25,30 +25,40 @@
 
 // Return true if it is possible to remove one element from the array in order to get a strictly increasing sequence, otherwise return false.
 function almostIncreasingSequence(sequence) {
-  var result = false;
-  var count = 0;
   var current, next, previous;
   
   for (var i = 0; i < sequence.length; i++) {
-    var temp = sequence.slice();
-    temp.splice(i, 1);
-    if (checkIncreasing(temp)) {
-      result = true;
-      break;
+    previous = -Infinity;
+    // console.log('i: ', i);
+    for (var j = 0; j < sequence.length; j++) {
+      if (j === i) {
+        previous = sequence[j - 1] || previous;
+        continue;
+      } else {
+
+        current = sequence[j];
+        next = sequence[j + 1];
+        // console.log('current: ', current, ' ', 'next: ', next);
+        if (next === undefined && current > previous) {
+          return true;
+        }
+        // check left
+        var nextEqualsSkip = (j+1 === i);
+        if (current >= next && !nextEqualsSkip) {
+          // console.log('here');
+          break;
+        } else if ((current <= previous || current >= next) && !nextEqualsSkip) {
+          // check right
+          break;
+        } 
+
+      }
     }
   }
 
-  return result;
+  return false;
 }
 
-function checkIncreasing(sequence) {
-  for (var i = 0; i < sequence.length; i++) {
-    if (sequence[i + 1] <= sequence[i]) {
-      return false;
-    }
-  }
-  return true;
-}
 
 var sequence = [1, 3, 2, 1];
 console.log(almostIncreasingSequence(sequence), ' should be false'); // false;
@@ -64,3 +74,31 @@ sequence = [1, 2, 3, 4, 99, 5, 6];
 console.log(almostIncreasingSequence(sequence), ' should be true');
 sequence = [1, 2, 1, 2];
 console.log(almostIncreasingSequence(sequence), ' should be false'); // false;
+
+
+//implementation that finishes 25/26 tests in codefights
+/*
+function almostIncreasingSequence(sequence) {
+  var current, next, previous;
+  
+  for (var i = 0; i < sequence.length; i++) {
+    var temp = sequence.slice();
+    temp.splice(i, 1);
+    if (checkIncreasing(temp)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function checkIncreasing(sequence) {
+  for (var i = 0; i < sequence.length; i++) {
+    if (sequence[i + 1] <= sequence[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+*/
+
